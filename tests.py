9a0397ea1af4cbb5ac54d1f19fc3bd36ba9052d5
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.net>
2018-10-20 14:23:07 +0200

    Stop using deprecated assertEquals function

--- tests.py
+++ tests.py
@@ -3,41 +3,41 @@ import shiftcipher
 
 class Test_TestEncode(unittest.TestCase):
     def test_encode_z_1(self):
-        self.assertEquals(shiftcipher.encode('z', 1), 'a')
+        self.assertEqual(shiftcipher.encode('z', 1), 'a')
 
     def test_encode_a_1(self):
-        self.assertEquals(shiftcipher.encode('a', 1), 'b')
+        self.assertEqual(shiftcipher.encode('a', 1), 'b')
 
     def test_encode_a_25(self):
-        self.assertEquals(shiftcipher.encode('a', 25), 'z')
+        self.assertEqual(shiftcipher.encode('a', 25), 'z')
 
     def test_encode_z_25(self):
-        self.assertEquals(shiftcipher.encode('z', 25), 'y')
+        self.assertEqual(shiftcipher.encode('z', 25), 'y')
 
     def test_encode_AbCdEfG_1(self):
-        self.assertEquals(shiftcipher.encode('AbCdEfG', 1), 'AcCeEgG')
+        self.assertEqual(shiftcipher.encode('AbCdEfG', 1), 'AcCeEgG')
 
     def test_encode_AbCdEfG_25(self):
-        self.assertEquals(shiftcipher.encode('AbCdEfG', 25), 'AaCcEeG')
+        self.assertEqual(shiftcipher.encode('AbCdEfG', 25), 'AaCcEeG')
 
 class Test_TestDecode(unittest.TestCase):
     def test_decode_z_1(self):
-        self.assertEquals(shiftcipher.decode('z', 1), 'y')
+        self.assertEqual(shiftcipher.decode('z', 1), 'y')
 
     def test_decode_a_1(self):
-        self.assertEquals(shiftcipher.decode('a', 1), 'z')
+        self.assertEqual(shiftcipher.decode('a', 1), 'z')
 
     def test_decode_a_25(self):
-        self.assertEquals(shiftcipher.decode('a', 25), 'b')
+        self.assertEqual(shiftcipher.decode('a', 25), 'b')
 
     def test_decode_z_25(self):
-        self.assertEquals(shiftcipher.decode('z', 25), 'a')
+        self.assertEqual(shiftcipher.decode('z', 25), 'a')
 
     def test_decode_AbCdEfG_1(self):
-        self.assertEquals(shiftcipher.decode('AbCdEfG', 1), 'AaCcEeG')
+        self.assertEqual(shiftcipher.decode('AbCdEfG', 1), 'AaCcEeG')
 
     def test_decode_AbCdEfG_25(self):
-        self.assertEquals(shiftcipher.decode('AbCdEfG', 25), 'AcCeEgG')
+        self.assertEqual(shiftcipher.decode('AbCdEfG', 25), 'AcCeEgG')
 
 if __name__ == '__shiftcipher__':
     unittest.shiftcipher()
